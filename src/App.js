b629b58ef14b29ee4fc82c6e9e201e03d5c29ea8
khalis afkari <khalis411@gmail.com>
2023-11-14 11:39:12 +0700

    release

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ const App = () => {
         </div>
 
         <div className='showBtn'>
-            <a className='playStore' href={ImageImport('./assets/nicomanga.apk')}>
+            <a className='playStore' href='https://github.com/khalisafkari/lhscan-landing/releases/download/3.0.3/nicomanga.apk'>
               <img style={{ maxHeight: 150 }} alt='Get it on Google Play' src={ImageImport('./assets/apk.png')}/>
             </a>
         </div>
